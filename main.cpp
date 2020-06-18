53badb8c54ed2bbfaafae853825060d0838d6eb8
Daniel Smith <dansmtn@gmail.com>
2020-06-17 20:57:51 -0400

    Basic print + add to records

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@
 
 using namespace std;
 
+struct sort_second {
+	bool operator()(const pair<string,int> &left, const pair<string,int> &right) {
+        return left.second < right.second;
+    }
+};
+
 void readCsv(ifstream &inFile, vector<pair<string,int>> *records);
 
 void mainMenu(vector<pair<string,int>> *records);
@@ -22,13 +28,16 @@ int main() {
 	recordsIn.open("records.list");
 	vector<pair<string, int>> *records = new vector<pair<string,int>>();
 	readCsv(recordsIn, records);
+
 	recordsIn.close();
-	//for(vector<pair<string, int>>::iterator it = records->begin(); it != records->end(); it++) {
-	//	cout << it->first << "\t" << it->second << endl;
-	//}
-	//ofstream recordsOut("records.list");
-	//recordsOut.close();
+	ofstream recordsOut("records.list");
+
 	mainMenu(records);
+
+	for(vector<pair<string, int>>::iterator it = records->begin(); it != records->end(); it++) {
+		recordsOut << it->first << "," << it->second << endl;
+	}
+	recordsOut.close();
 	return 0;
 }
 
@@ -51,29 +60,76 @@ void mainMenu(vector<pair<string,int>> *records) {
 	options[4] = deleteRecord;
 
     do {
-        cout << "Main Menu" << endl << "Choose one of the options below" << endl;
+        cout << "Main Menu" << endl << "Choose one of the options below and then hit ENTER. Or press 0 + ENTER to exit" << endl;
         cout << "1. Upcoming Maintenance Required" << endl;
         cout << "2. All Maintenance Records" << endl;
         cout << "3. Update Maintenance Record" << endl;
         cout << "4. Add Maintenance Record" << endl;
         cout << "5. Delete Maintenance Record" << endl << endl;
         cin >> choice;
-    } while(choice < 1 || choice > 5);
-	options[choice-1](records);
+    } while(choice < 0 || choice > 5);
+	if(choice > 0 && choice < 6) options[choice-1](records);
 }
 
 void upcomingMaintenance(vector<pair<string,int>> *records) {
-	cout << "upcoming" << endl;
+	sort(records->begin(), records->end(), sort_second());
+	if(records->size() >= 5) {
+		for(int i = 0; i < 5; i++) {
+			cout << endl << records->at(i).second << "\t" << records->at(i).first;
+		}
+	}
+	else {
+		for(vector<pair<string, int>>::iterator it = records->begin(); it != records->end(); it++) {
+			cout << endl << it->second << "\t" << it->first;
+		}
+	}
+	cout << endl << endl << "Press 1 to go back to Main Menu. Press any other key + ENTER to exit." << endl;	
+	int choice;
+	cin >> choice;
+	if(choice == 1) mainMenu(records);
 }
 void allRecords(vector<pair<string,int>> *records) {
-	cout << "all" << endl;
+	sort(records->begin(), records->end());
+	if(records->size() >= 5) {
+		for(int i = 0; i < 5; i++) {
+			cout << endl << records->at(i).second << "\t" << records->at(i).first;
+		}
+	}
+	else {
+		for(vector<pair<string, int>>::iterator it = records->begin(); it != records->end(); it++) {
+			cout << endl << it->second << "\t" << it->first;
+		}
+	}
+	cout << endl << endl << "Press 1 to go back to Main Menu. Press any other key + ENTER to exit." << endl;	
+	int choice;
+	cin >> choice;
+	if(choice == 1) mainMenu(records);
 }
 void updateRecord(vector<pair<string,int>> *records) {
 	cout << "update" << endl;
+	cout << endl << "Press 1 to go back to Main Menu. Press any other key + ENTER to exit." << endl;	
+	int choice;
+	cin >> choice;
+	if(choice == 1) mainMenu(records);
 }
 void addRecord(vector<pair<string,int>> *records) {
-	cout << "add" << endl;
+	string name;
+	int miles;
+	cout << "Type the name of the maintenance being performed and then hit ENTER" << endl;
+	getline(cin.ignore(), name);
+	cout << "Type the number of miles at which the maintenance needs to be performed next and then hit ENTER" << endl;
+	cin >> miles;
+	records->push_back(make_pair(name, miles));
+	cout << "Maintenance record successfully added..." << endl;
+	cout << endl << "Press 1 to go back to Main Menu. Press any other key + ENTER to exit." << endl;	
+	int choice;
+	cin >> choice;
+	if(choice == 1) mainMenu(records);
 }
 void deleteRecord(vector<pair<string,int>> *records) {
 	cout << "delete" << endl;
+	cout << endl << "Press 1 to go back to Main Menu. Press any other key + ENTER to exit." << endl;	
+	int choice;
+	cin >> choice;
+	if(choice == 1) mainMenu(records);
 }
